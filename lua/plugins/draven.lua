8786684a0e9ee140bd8d933113049234243c1b05
James Lindsly <james.lindsly@exxeta.com>
2026-09-14 20:15:37 +0200

    [ADD] Draven AI review plugin

---
+++ lua/plugins/draven.lua
@@ -0,0 +1,8 @@
+return {
+  'ploMP4/draven.nvim',
+  cmd = { 'Draven', 'DravenToggle', 'DravenStatus' },
+  keys = {
+    { '<leader>ar', '<cmd>Draven<cr>', desc = '[A]I [R]eview' },
+  },
+  opts = {},
+}
